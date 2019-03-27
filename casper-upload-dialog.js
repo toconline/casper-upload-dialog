87473104bbd87bfe9942ed8deb2e4adef251e2d0
André Silva <andreemanuel.1993@gmail.com>
2019-03-27 11:59:44 +0000

    Fixed formatting of the component's license

--- casper-upload-dialog.js
+++ casper-upload-dialog.js
@@ -1,20 +1,22 @@
 /*
-Copyright (c) 2014-2016 Cloudware S.A. All rights reserved.
-
-This file is part of casper-upload-dialog.
-
-casper-upload-dialog is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-
-casper-upload-dialog  is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with casper-upload-dialog.  If not, see <http://www.gnu.org/licenses/>.
-*/
+  - Copyright (c) 2014-2016 Cloudware S.A. All rights reserved.
+  -
+  - This file is part of casper-upload-dialog.
+  -
+  - casper-upload-dialog is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as published by
+  - the Free Software Foundation, either version 3 of the License, or
+  - (at your option) any later version.
+  -
+  - casper-upload-dialog  is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with casper-upload-dialog.  If not, see <http://www.gnu.org/licenses/>.
+  -
+ */
 
 class CasperUploadDialog extends CasperWizard {
   static get template() {
